d32a0e10f22d65dbe2f7bab7b991c3ab7a27fae3
tevin davis <aod.td1105@gmail.com>
2020-03-19 21:14:47 -0400

    Added delete function to MovieCard

--- client/src/components/MovieUpdate.js
+++ client/src/components/MovieUpdate.js
@@ -1,21 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import MovieCard from '../Movies/MovieCard';
 import api from '../utils/api';
 
 export default function MovieUpdate(props) {
+
     const [movie, setMovie] = useState({
-        id: 0,
+        id: 1,
         title: "",
         director: "",
         metascore: 0,
         stars: []
     })
 
+    useEffect(() => {
+        api()
+            .get(`/api/movies/${props.match.params.id}`)
+                .then(res => {
+                    setMovie(res.data)
+                }).catch(err => {
+                    console.log(err)
+                })
+    }, [props.match.params.id])
+
     const handleSubmit = event => {
         event.preventDefault()
 
-        api().put(`/update-movie/${movie.id}`, movie)
+        api().put(`/api/movies/${movie.id}`, movie)
             .then(res => {
                 props.history.push("/")
+                console.log(res)
             }).catch(err => {
                 console.log(err)
             })
@@ -29,35 +42,39 @@ export default function MovieUpdate(props) {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                name="title"
-                placeholder="Title"
-                value={movie.title}
-                onChange={handleChange}
-            />
-            <input
-                type="text"
-                name="director"
-                placeholder="Director"
-                value={movie.director}
-                onChange={handleChange}
-            />
-            <input
-                type="number"
-                name="metascore"
-                placeholder="Metascore"
-                value={movie.metascore}
-                onChange={handleChange}
-            />
-            <input
-                type="text"
-                name="stars"
-                placeholder="Stars"
-                value={movie.stars}
-                onChange={handleChange}
-            />
-        </form>
+        <>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="Title"
+                    value={movie.title}
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    name="director"
+                    placeholder="Director"
+                    value={movie.director}
+                    onChange={handleChange}
+                />
+                <input
+                    type="number"
+                    name="metascore"
+                    placeholder="Metascore"
+                    value={movie.metascore}
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    name="stars"
+                    placeholder="Stars"
+                    value={movie.stars}
+                    onChange={handleChange}
+                />
+                <button type="submit">Edit</button>
+            </form>
+            <MovieCard movie={movie} />
+        </>
     )
 }
